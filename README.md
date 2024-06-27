2fdd81fc6d383d6497aad81bc0f08433a6f232f9
Paradise <112849433+P4radis3@users.noreply.github.com>
2024-06-27 15:16:43 +0300

    Initial Commit.

--- README.md
+++ README.md
@@ -1,1 +1,152 @@
-# telegram-bot
+<a name="readme-top"></a>
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/P4radis3/Telegram-Bot">
+  </a>
+
+<h3 align="center">Paradise's Notes</h3>
+
+  <p align="center">
+    <br />
+    <a href="https://github.com/P4radis3/Telegram-Bot">View Demo</a>
+    ·
+    <a href="https://github.com/P4radis3/Telegram-Bot/issues/new?labels=bug&template=bug-report---.md">Report Bug</a>
+    ·
+    <a href="https://github.com/P4radis3/Telegram-Bot/issues/new?labels=enhancement&template=feature-request---.md">Request Feature</a>
+  </p>
+</div>
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+<!-- [![Product Name Screen Shot][product-screenshot]](https://example.com)
+
+Here's a blank template to get started: To avoid retyping too much info. Do a search and replace with your text editor for the following: `github_username`, `repo_name`, `twitter_handle`, `linkedin_username`, `email_client`, `email`, `project_title`, `project_description` -->
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+### Built With
+
+* <a href="https://nodejs.org/en" target="_blank"><img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" width="120" alt="NodeJS" style="max-width: 100%;"></a>
+* <a href="https://www.npmjs.com/package/koa" target="_blank"><img src="https://github.com/koajs/koa/blob/HEAD/docs/logo.png?raw=true" width="120" alt="KOA" style="max-width: 100%;"></a>
+* <a href="https://www.npmjs.com/package/node-telegram-bot-api" target="_blank"><img src="https://logos-download.com/wp-content/uploads/2016/07/Telegram_5.x_version_2019_Logo.png" width="120" alt="Telegram" style="max-width: 100%;"></a>
+* <a href="https://www.npmjs.com/package/@babel/preset-env" target="_blank"> <img src="https://raw.githubusercontent.com/babel/logo/master/babel.png" width="120" alt="Preset ENV" style="max-width: 100%;"></a>
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+## Getting Started
+
+This is an example of how you may give instructions on setting up your project locally.
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+This is an example of how to list things you need to use the software and how to install them.
+* npm
+  ```sh
+  npm install npm@latest -g
+  ```
+
+### Installation
+
+1. Get a free API Key at [https://example.com](https://example.com)
+2. Clone the repo
+   ```sh
+   git clone https://github.com/P4radis3/Telegram-Bot.git
+   ```
+3. Install NPM packages
+   ```sh
+   npm install
+   ```
+4. Enter your TOKEN and Port (which should be 3000 by default) in a config-file named `default.json5`
+   ```js
+   "port": 3000,
+    "token": "YOUR_TOKEN_HERE"
+   ```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- ROADMAP -->
+## Roadmap
+
+See the [open issues](https://github.com/P4radis3/Telegram-Bot/issues) for a full list of proposed features (and known issues).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/YourFeatureName`)
+3. Commit your Changes (`git commit -m 'Add some YourFeatureName'`)
+4. Push to the Branch (`git push origin feature/YourFeatureName`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Paradise - [@twitter_handle](https://twitter.com/twitter_handle)
+
+Project Link: [https://github.com/P4radis3/Telegram-Bot](https://github.com/P4radis3/Telegram-Bot)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
---
+++ index.js
@@ -0,0 +1,206 @@
+const TelegramBot = require('node-telegram-bot-api');
+const config = require('config');
+const fs = require('fs');
+const path = require('path');
+
+const TOKEN = config.get('token');
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+const filePath = path.join(__dirname, 'userdata.json');
+
+let userData = {};
+if (fs.existsSync(filePath)) { const userDataFile = fs.readFileSync(filePath); userData = JSON.parse(userDataFile); }
+
+const saveToDos = () => { fs.writeFileSync(filePath, JSON.stringify(userData, null, 2)); };
+bot.on('callback_query', query => { bot.answerCallbackQuery(query.id, `${query.data}`); });
+
+bot.onText(/\/start/, (message) => {
+    const { chat: { id }, from: { username } } = message;
+    bot.sendMessage(id, `Hello, ${username}! Thanks for using my bot! Have a look through the buttons labeled 'IMPORTANT'`, {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: 'My personal GitHub',
+                        url: 'https://github.com/p4radis3'
+                    },
+                    {
+                        text: 'Reach out via EMail.',
+                        url: 'https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRlQQFBzdkwtrNnZXqrRJCXfMnkzbHFqmxnPsbJMRmhfsCNzhQrJQszfwtfMTDhllxVWPQv'
+                    }
+                ],
+                [
+                    {
+                        text: 'IMPORTANT #1!',
+                        callback_data: 'Please, be aware! The bot is still in development!'
+                    },
+                    {
+                        text: 'IMPORTANT #2!',
+                        callback_data: 'If you have any recommendations, please contact me!'
+                    }
+                ],
+                [
+                    {
+                        text: 'Contribute to the bot.',
+                        url: 'https://github.com/P4radis3/Telegram-Bot'
+                    }
+                ]
+            ]
+        }
+    });
+});
+
+bot.onText(/^\/add$/, (message) => {
+    const userId = message.from.id;
+    bot.sendMessage(message.chat.id, `Enter a to-do item you want to add to your list:`);
+
+    bot.once('message', (inputMessage) => {
+        const todoItem = inputMessage.text;
+        if (!todoItem) {
+            bot.sendMessage(message.chat.id, `You cannot add an empty item to the to-do list.`); return;
+        }
+
+        const isLink = todoItem.startsWith('http');
+        if (!userData[userId]) { userData[userId] = []; }
+        userData[userId].push({ text: todoItem, isLink: isLink, completed: false });
+        saveToDos();
+        bot.sendMessage(message.chat.id, `Added "${todoItem}" to your to-do list.`);
+    });
+});
+
+
+bot.onText(/\/list/, (message) => {
+
+    const userId = message.from.id; const userTodoItems = userData[userId] || [];
+    let todoList = 'Your To-Do List:\n';
+    userTodoItems.forEach(item => { if (item.isLink) { todoList += `🔗 ${item.text}\n`; } else { todoList += `📝 ${item.text}\n`; } })
+    bot.sendMessage(message.chat.id, todoList, { parse_mode: 'HTML' });
+
+});
+
+process.on('SIGINT', () => { saveToDos(); process.exit(); });
+let lastDeleted = {};
+const deleteItem = (chatId, todoItems) => {
+
+    const keyboard = {
+        keyboard: todoItems.map((item, index) => [{ text: `${index + 1}. ${item.text}` }]),
+        resize_keyboard: true,
+        one_time_keyboard: true
+    };
+
+    bot.sendMessage(chatId, 'Select an item to delete:', { reply_markup: keyboard });
+
+};
+
+const undoDelete = (chatId) => {
+
+    const keyboard = { inline_keyboard: [[{ text: 'Undo and retrieve the last item you deleted.', callback_data: 'Undid deletion.' }]] };
+    bot.sendMessage(chatId, 'To undo the deletion, click the button below:', { reply_markup: keyboard });
+
+};
+
+bot.onText(/\/delete/, (message) => {
+
+    const userId = message.from.id;
+    const userToDoList = userData[userId] || [];
+    if (userToDoList.length === 0) { bot.sendMessage(message.chat.id, 'Your to-do list is empty.'); } else { deleteItem(message.chat.id, userToDoList); }
+
+    bot.once('message', (deleteMessage) => {
+
+        const index = parseInt(deleteMessage.text) - 1;
+        if (!isNaN(index) && index >= 0 && index < userToDoList.length) {
+
+            lastDeleted[userId] = userToDoList.splice(index, 1)[0];
+            saveToDos();
+            bot.sendMessage(message.chat.id, `Deleted "${lastDeleted[userId].text}" from your to-do list.`);
+            undoDelete(message.chat.id);
+
+        } /*else {
+            bot.sendMessage(message.chat.id, 'Invalid selection. Please select a valid to-do item to delete.');
+        }*/
+    });
+});
+
+bot.on('callback_query', query => {
+    if (query.data === 'Undid deletion.') {
+
+        const userId = query.from.id;
+        if (lastDeleted[userId]) {
+
+            userData[userId].push(lastDeleted[userId]);
+            delete lastDeleted[userId];
+            saveUserDataToFile();
+            bot.answerCallbackQuery(query.id, 'Last deletion undone.');
+
+        } else { bot.answerCallbackQuery(query.id, 'No deletion to undo.'); }
+    }
+});
+
+
+const editSessions = {};
+bot.onText(/^\/edit/, (message) => {
+    const userId = message.from.id;
+    const userToDoList = userData[userId] || [];
+
+    if (userToDoList.length === 0) {
+        bot.sendMessage(message.chat.id, 'Your to-do list is empty.');
+    } else {
+        userEdit(message.chat.id, userId, userToDoList);
+    }
+});
+
+const userEdit = (chatId, userId, todoItems) => {
+    const editSessionId = `${userId}_${Date.now()}`;
+    editSessions[editSessionId] = { userId, todoItems };
+
+    const keyboard = {
+        keyboard: todoItems.map((item, index) => [{
+            text: `${index + 1}. ${item.isLink ? '🔗' : '📝'} ${item.text}`
+        }]),
+        resize_keyboard: true,
+        one_time_keyboard: true
+    };
+
+    bot.sendMessage(chatId, 'Select an item to edit:', { reply_markup: keyboard });
+
+    userData[userId].editSessionId = editSessionId;
+    saveToDos();
+    bot.once('message', (editMessage) => {
+        edit(editMessage, editSessionId);
+    });
+
+};
+
+const edit = (editMessage, editSessionId) => {
+    const [userId, timestamp] = editSessionId.split('_');
+    const todoItems = editSessions[editSessionId].todoItems;
+    const index = parseInt(editMessage.text) - 1;
+
+    if (!isNaN(index) && index >= 0 && index < todoItems.length) {
+        const selectedItem = todoItems[index];
+        bot.sendMessage(userId, `Enter the new text for "${selectedItem.text}":`).then(() => {
+            bot.once('message', (newTextMessage) => { newTextFromUser(newTextMessage, editSessionId, selectedItem); });
+        });
+    } else {
+        bot.sendMessage(userId, 'Invalid selection. Please select a valid to-do item to edit.');
+    }
+};
+
+const newTextFromUser = (newTextMessage, editSessionId, selectedItem) => {
+
+    const [userId, timestamp] = editSessionId.split('_');
+    const newText = newTextMessage.text;
+    selectedItem.text = newText;
+
+    if (selectedItem.isLink && !newText.startsWith('http')) {
+        selectedItem.isLink = false;
+    } else if (!selectedItem.isLink && newText.startsWith('http')) {
+        selectedItem.isLink = true;
+    }
+
+    bot.sendMessage(userId, `Successfully edited "${selectedItem.text}".`);
+    delete editSessions[editSessionId];
+    delete userData[userId].editSessionId;
+    saveToDos();
+
+};
